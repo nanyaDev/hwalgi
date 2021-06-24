67cc60af7f5ab8167e87b8510cf058c16bba9578
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-06-24 10:27:31 +0530

    Implement auth with Firebase

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,59 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import { useAuth } from '../lib/auth';
 
 export default function Home() {
+  const auth = useAuth();
+
+  // testing code: start
+  const handleSignup = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    auth.signup(email, password);
+  };
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    auth.signin(email, password);
+  };
+  // testing code: end
+
   return (
     <div>
       <Head>
-        <title>Hwalgi // Dev</title>
+        <title>Hwalgi // Development</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello from Hwalgi</h1>
+      {/* testing code: start */}
+      <h1>Auth Skeleton</h1>
+
+      <div>Signed in as: {auth.user?.email}</div>
+
+      <form onSubmit={handleSignup}>
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">Sign Up</button>
+      </form>
+      <br />
+
+      <form onSubmit={handleSignin}>
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">Sign In</button>
+      </form>
+      <br />
+
+      <button type="button" onClick={() => auth.signout()}>
+        Sign Out
+      </button>
+      {/* testing code: end */}
     </div>
   );
 }
