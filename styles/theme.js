fb4983a975a8eaca7da234f00abbb5e004606bd7
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-07-17 13:45:50 +0530

    Remove outline when button is focused

--- styles/theme.js
+++ styles/theme.js
@@ -1,9 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  fonts: {
-    body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
-  },
   styles: {
     global: {
       html: {
@@ -13,6 +10,21 @@ const theme = extendTheme({
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh',
+      },
+    },
+  },
+  fonts: {
+    body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        _focus: {
+          boxShadow: 'none',
+        },
+        _focusVisible: {
+          boxShadow: 'outline',
+        },
       },
     },
   },
