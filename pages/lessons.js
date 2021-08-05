d5eec6912101b948a6f4bcb9e3557a18df4d513a
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-08-05 14:46:10 +0530

    Layout flashcards

--- pages/lessons.js
+++ pages/lessons.js
@@ -1,28 +1,14 @@
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import { Flex, Heading, Input } from '@chakra-ui/react';
 
 import AuthCheck from '@/components/AuthCheck';
 import Navbar from '@/components/Navbar';
 
-const Lessons = () => {
+const Reviews = () => {
   return (
     <AuthCheck>
       <Navbar />
-      <Flex direction="column" grow={1} p={12} m={12} bg="gray.200">
-        <Flex grow={1} direction="column" justify="center" align="center">
-          <Heading>Hangul</Heading>
-          <Text>English</Text>
-        </Flex>
-        <Flex grow={2}>
-          <Flex grow={2}>
-            <Text>Mnemonic</Text>
-          </Flex>
-          <Flex grow={1}>
-            <Text>Mouth Shape</Text>
-          </Flex>
-        </Flex>
-      </Flex>
     </AuthCheck>
   );
 };
 
-export default Lessons;
+export default Reviews;
--- pages/reviews.js
+++ pages/reviews.js
@@ -1,16 +1,87 @@
-import { Flex, Heading, Input } from '@chakra-ui/react';
+import { Box, Button, Flex, Input, Kbd, Switch, Text } from '@chakra-ui/react';
 
 import AuthCheck from '@/components/AuthCheck';
 import Navbar from '@/components/Navbar';
 
-const Reviews = () => {
+// todo: abstract background to shell
+const Lessons = () => {
   return (
     <AuthCheck>
       <Navbar />
-      <Heading>Hangul</Heading>
-      <Input />
+      <Shell>
+        <Card>
+          <Box>
+            <Text
+              fontSize="6xl"
+              fontWeight="medium"
+              color="gray.600"
+              textAlign="center"
+              mb={6}
+            >
+              활기
+            </Text>
+            <Input
+              size="lg"
+              variant="flushed"
+              textAlign="center"
+              mb={12}
+              placeholder="Your Response"
+            />
+          </Box>
+          <Buttons />
+        </Card>
+      </Shell>
     </AuthCheck>
   );
 };
 
-export default Reviews;
+const Shell = ({ children }) => (
+  <Flex
+    grow={1}
+    direction="column"
+    justify="center"
+    align="center"
+    bg="gray.50"
+  >
+    {children}
+  </Flex>
+);
+
+const Card = ({ children }) => (
+  <Flex
+    h="450px"
+    w="700px"
+    direction="column"
+    bg="white"
+    borderRadius="xl"
+    boxShadow="base"
+    border="1px"
+    borderColor="gray.100"
+  >
+    <Flex justify="space-between" p={4}>
+      <Switch size="sm" />
+      <Text fontSize="md" fontWeight="bold" color="gray.500">
+        Recall
+      </Text>
+      <Text fontSize="sm" fontWeight="bold" color="gray.400">
+        19/82
+      </Text>
+    </Flex>
+    <Flex grow={1} px={32} pb={8} direction="column" justify="center">
+      {children}
+    </Flex>
+  </Flex>
+);
+
+const Buttons = () => (
+  <Flex justify="space-between">
+    <Button variant="ghost" border="1px" colorScheme="purple">
+      Retry <Kbd ml={2}>Esc</Kbd>
+    </Button>
+    <Button variant="ghost" border="1px" colorScheme="blue">
+      Submit <Kbd ml={2}>↵</Kbd>
+    </Button>
+  </Flex>
+);
+
+export default Lessons;
