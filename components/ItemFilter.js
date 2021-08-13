9205c4c1473b54b8d84a7e5265fb7a694db5d44e
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-08-13 19:11:56 +0530

    Add pagination controls to item page

--- components/ItemFilter.js
+++ components/ItemFilter.js
@@ -1,5 +1,6 @@
 // prettier-ignore
-import { HStack, Select, Text, } from '@chakra-ui/react';
+import { Flex, HStack, IconButton, Select, Text } from '@chakra-ui/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 // todo: find a way to reconcile CatalogFilter and ItemFilter
 const ItemFilter = ({ updateFilter }) => {
@@ -17,28 +18,43 @@ const ItemFilter = ({ updateFilter }) => {
   ];
 
   return (
-    <HStack spacing={8}>
-      {filters.map(({ name, values, selected }) => (
-        <HStack spacing={3} key={name}>
-          <Text fontWeight="medium">{name}</Text>
-          <Select
-            w="fit-content"
-            onChange={(e) =>
-              updateFilter(
-                name.toLowerCase().split(' ')[0],
-                e.target.value.toLowerCase()
-              )
-            }
-          >
-            {values.map((val) => (
-              <option key={val} value={val} selected={val === selected}>
-                {val}
-              </option>
-            ))}
-          </Select>
-        </HStack>
-      ))}
-    </HStack>
+    <Flex justify="space-between">
+      <HStack spacing={8}>
+        {filters.map(({ name, values, selected }) => (
+          <HStack spacing={3} key={name}>
+            <Text fontWeight="medium">{name}</Text>
+            <Select
+              w="fit-content"
+              onChange={(e) =>
+                updateFilter(
+                  name.toLowerCase().split(' ')[0],
+                  e.target.value.toLowerCase()
+                )
+              }
+            >
+              {values.map((val) => (
+                <option key={val} value={val} selected={val === selected}>
+                  {val}
+                </option>
+              ))}
+            </Select>
+          </HStack>
+        ))}
+      </HStack>
+      <Flex align="center">
+        <Text mr={4}>51 - 100 of 783</Text>
+        <IconButton
+          variant="ghost"
+          aria-label="Previous Page"
+          icon={<ChevronLeftIcon boxSize="1.5em" />}
+        />
+        <IconButton
+          variant="ghost"
+          aria-label="Next Page"
+          icon={<ChevronRightIcon boxSize="1.5em" />}
+        />
+      </Flex>
+    </Flex>
   );
 };
 
