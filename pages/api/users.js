3b4ed9f63dda4e7785d5d76811b04865debd4a3c
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-09-06 13:52:47 +0530

    Remove GET endpoint for /users

--- pages/api/users.js
+++ pages/api/users.js
@@ -15,18 +15,6 @@ const handler = async (req, res) => {
     await firestore.collection('users').doc(uid).set(data);
     res.status(200).json(data);
   }
-
-  // ! authenticate this request
-  if (req.method === 'GET') {
-    const snapshot = await firestore.collection('users').get();
-    const users = [];
-
-    snapshot.forEach((doc) => {
-      users.push(doc.data());
-    });
-
-    res.status(200).json(users);
-  }
 };
 
 export default handler;
