2103be317f8c961089d99b2e3789a8faddf7ac15
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-09-07 14:02:48 +0530

    Implement GET on reviews API endpoint

--- pages/api/reviews.js
+++ pages/api/reviews.js
@@ -1,10 +1,24 @@
+import { fireauth, firestore } from '@/utils/fireadmin';
+
 const handler = async (req, res) => {
   if (req.method === 'GET') {
-    res.status(200).json({});
+    const { uid } = await fireauth.verifyIdToken(req.headers.token);
+
+    const reviewsRef = firestore
+      .collection('users')
+      .doc(uid)
+      .collection('reviews');
+    const reviewsSnapshot = await reviewsRef.get();
+    const reviews = reviewsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json(reviews);
   }
 
   if (req.method === 'POST') {
-    res.status(200).json({});
+    res.status(200).send();
   }
 };
 
